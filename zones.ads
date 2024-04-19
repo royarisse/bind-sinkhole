b9f6f63975f68f4bad4796a9798388b25896143c
Roy Arisse <roy@perfacilis.com>
2024-04-19 23:11:04 +0200

    Point to proper lists and add some extra zones Fix: URLS pointed to old lists, fail! Feature: Added some extra zones.

--- zones.ads
+++ zones.ads
@@ -1,2 +1,5 @@
 zone "metrics.ubuntu.com" { type master; notify no; file "/etc/bind/blocked.zone"; };
 zone "unityads.unity3d.com" { type master; notify no; file "/etc/bind/blocked.zone"; };
+zone "admost.com" { type master; notify no; file "/etc/bind/blocked.zone"; };
+zone "applvn.com" { type master; notify no; file "/etc/bind/blocked.zone"; };
+zone "applovin.com" { type master; notify no; file "/etc/bind/blocked.zone"; };
