3d6850d40f203ad04eb942e6febabd87bb958dbe
Roy Arisse <roy@perfacilis.com>
2025-06-03 11:25:51 +0200

    Switch to hosts.allow and hosts.block file Feature: Add simple hosts.allow file for sites tha should be whitelisted. Feature: Add simple hosts.deny file for sites that should be blacklisted. Feature: Rename `bind-sinkhole` to `cron` for more obvious naming in repo. Fix: Minor typoes in cron to make it all run.

--- zones.ads
+++
@@ -1,5 +0,0 @@
-zone "metrics.ubuntu.com" { type master; notify no; file "/etc/bind/blocked.zone"; };
-zone "unityads.unity3d.com" { type master; notify no; file "/etc/bind/blocked.zone"; };
-zone "admost.com" { type master; notify no; file "/etc/bind/blocked.zone"; };
-zone "applvn.com" { type master; notify no; file "/etc/bind/blocked.zone"; };
-zone "applovin.com" { type master; notify no; file "/etc/bind/blocked.zone"; };
